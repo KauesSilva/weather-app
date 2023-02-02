670d2d63731b082d4d0bb59e43074ef76456566c
KauesSilva <kauessilva00@gmail.com>
2023-02-02 13:23:31 -0300

    Code refactored to OOP

--- main.js
+++ main.js
@@ -1,50 +1,51 @@
 class Weather {
-    constructor(locationName, temperature) {
-        this._locationName = locationName,
-        this._temperature = Math.trunc(temperature) + '°C'
-    }
-    get locationName() {
-        return this._locationName;
-    }
-    set locationName(newLocationName) {
-        this._locationName = newLocationName;
-    }
-    get temperature() {
-        return this._temperature;
-    }
-    set temperature(newTemperature) {
-        this._temperature = newTemperature;
-    }
-};
+  constructor(locationName, temperature, feelsLike, tempMin, tempMax) {
+    this.locationName = locationName;
+    this.temperature = Math.trunc(temperature) + '°C';
+    this.feelsLike = feelsLike;
+    this.tempMin = tempMin;
+    this.tempMax = tempMax;
+  }
+}
 
-//Requesting openWeatherAPI data//
-async function requestWeather(locationName) {
-    const api_key = '738b4e44b882a8119a242f281a4c0d0e';
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationName}&appid=${api_key}&units=metric`);
-    let data = await response.json();
-    return data.cod != 200 ? Swal.fire({
+class WeatherService {
+  async requestWeather(locationName) {
+    const apiKey = '738b4e44b882a8119a242f281a4c0d0e';
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${locationName}&appid=${apiKey}&units=metric`);
+      const data = await response.json();
+      if (data.cod !== 200) {
+        throw new Error(data.message);
+      }
+      return data;
+    } catch (error) {
+      Swal.fire({
         icon: 'error',
         title: 'Ops...',
-        text: data.message,
+        text: error.message,
         scrollbarPadding: false
-    }) : data;
-};
-
-//Showing openWeatherAPI data//
-async function showWeather() {
-    let data = await requestWeather(document.querySelector("#searchInp").value);
-    weather = new Weather(data.name, data.main.temp);
-    //Activating containter transitions with jQuery//
-    $(".container").addClass("active");
-    $(".forecast").css("display", "flex");
-    console.log(data.weather);
-    //Setting dinamic weather icon based on wheather name(rain, clear, snow...)//
-    if (data.weather[0].id >= 701 && data.weather[0].id <= 781) {
-        document.querySelector(".weather-img").src = "./assets/weather-icons/Haze.svg";
-    } else {
-        document.querySelector(".weather-img").src = `./assets/weather-icons/${data.weather[0].main}.svg`;
+      });
     }
-    document.querySelector(".location-name").innerHTML = weather.locationName;
-    document.querySelector(".location-temp").innerHTML = weather.temperature;
+  }
+
+  async showWeather(locationName) {
+    try {
+      const data = await this.requestWeather(locationName);
+      const weather = new Weather(data.name, data.main.temp, data.main.feels_like, data.main.temp_min, data.main.temp_max);
+      $(".container").addClass("active");
+      $(".forecast").css("display", "flex");
+      if (data.weather[0].id >= 701 && data.weather[0].id <= 781) {
+        document.querySelector(".weather-img").src = "./assets/weather-icons/Haze.svg";
+      } else {
+        document.querySelector(".weather-img").src = `./assets/weather-icons/${data.weather[0].main}.svg`;
+      }
+      document.querySelector(".location-name").innerHTML = weather.locationName;
+      document.querySelector(".location-temp").innerHTML = weather.temperature;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
-document.querySelector("#searchBtn").addEventListener("click", showWeather);
+
+const weatherService = new WeatherService();
+document.querySelector("#searchBtn").addEventListener("click", () => weatherService.showWeather(document.querySelector("#searchInp").value));
